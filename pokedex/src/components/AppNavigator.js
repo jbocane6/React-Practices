0cbaa96b92720f91c086e9df53604b076a073d6a
Juan Bocanegra <jbocane6@gmail.com>
2022-03-06 11:35:05 -0500

    [UPDATE] Designing the navigation bar with material-ui

--- pokedex/src/components/AppNavigator.js
+++ pokedex/src/components/AppNavigator.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { colors, Toolbar, Typography } from '@material-ui/core'
+import { AppBar } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import { Link } from 'react-router-dom'
 
-export default function AppNAvigator() {
+const useStyles = makeStyles( (theme) => ({
+    AppBar: {
+        backgroundColor: "black"
+    },
+    link: {
+        textDecoration: 'none'
+    },
+    title: {
+        cursor: 'pointer',
+        color: 'white'
+    }
+}))
+
+export default function AppNavigator() {
+    const classes = useStyles()
   return (
-    <div>AppNAvigator</div>
+    <AppBar className={classes.AppBar} position='fixed' variant='h6'>
+        <Toolbar>
+            <Link to="/" className={classes.link}>
+                <Typography className={classes.title}>Pokedex</Typography>
+            </Link>
+        </Toolbar>
+    </AppBar>
   )
 }
