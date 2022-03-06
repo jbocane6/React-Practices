d216cbe74186bc7e02dcc136dc823ca587859453
Juan Bocanegra <jbocane6@gmail.com>
2022-03-06 12:03:43 -0500

    [UPDATE] Using the pokemon API to get the pokedex data with axios

--- pokedex/src/containers/Pokedex.js
+++ pokedex/src/containers/Pokedex.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Box, CircularProgress } from '@material-ui/core'
+import axios from 'axios'
+import { IMAGE_API_URL, POKEMON_API_URL } from '../config'
 
 export default function Pokedex() {
+    const [pokemonData, setPokemonData] = useState(null)
+    useEffect(() => {
+        axios.get(POKEMON_API_URL + "?limit=800").then((response) => {
+            if (response.status >= 200 && response.status < 300){
+                const { results } = response.data
+                let newPokemonData = []
+                results.forEach((pokemon, index) => {
+                    index++
+                    let pokemonObject = {
+                        id: index,
+                        url: IMAGE_API_URL + index + ".png",
+                        name:pokemon.name
+                    }
+                    newPokemonData.push(pokemonObject)
+                });
+                setPokemonData(newPokemonData)
+            }
+        }) 
+    }, [])
   return (
-    <div>
-        <h1>Hello World</h1>
-    </div>
+    <Box>
+        {pokemonData ? pokemonData.map((pokemon) => {
+            return <h1>{pokemon.name}</h1>
+        }) : <CircularProgress style={{ marginTop: 100}}/>}
+    </Box>
   )
 }
